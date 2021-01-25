cef0dc7efbd9b9db614c282c067d8696afe16376
tokagenosuke <akn_0905@yahoo.co.jp>
2021-01-25 16:35:12 +0900

    test

---
+++ rember.scm
@@ -0,0 +1,15 @@
+(define atom?
+  (lambda (x)
+    (and (not (null? x)) (not (pair? x)))))
+
+(define rember_star
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+      (cond
+        ((eq? (car l) a) (rember_star a (cdr l)))
+        (else (cons (car l) (rember_star a (cdr l))))))
+      (else (cons (rember_star a (car l)) (rember_star a (cdr l)))))))
+
+(print (rember_star 'cup '((coffee) cup ((tea) cup) (and (hick)) cup)))
